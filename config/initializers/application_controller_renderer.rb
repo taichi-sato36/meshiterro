6a178c7bf778c708c0bd5a30af9be651abaf3113
Taichi Satou <taichi@mpb.local>
2020-04-18 04:03:00 +0900

    [finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '1d014c2636b9c4ebd4fec23dfa028c4e320ec3d70efaa4d5f18589891f26b3c9f090120f4330ed86ee944a83496ce76d5813e32a4c46d8b8bfb77d91866c981b'
